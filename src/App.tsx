cc145978bb520b6cc734df5621511757ad7adb7f
Volodymyr Alputov <alputov1998@gmail.com>
2025-12-08 19:04:55 +0200

    feat: add scroll to top on route change
    - Create ScrollToTop component that scrolls to top when route changes
    - Add ScrollToTop to App.tsx to handle all page navigation
    - Ensures pages always start at the top when navigating

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
+import { ScrollToTop } from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import AuthPage from "./pages/AuthPage";
 import ChatPage from "./pages/ChatPage";
@@ -21,6 +22,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={ <Index /> } />
           <Route path="/auth" element={ <AuthPage /> } />
