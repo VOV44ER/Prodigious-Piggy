50fd83e353cd15dc88a612c70db878a8adc2d03e
Volodymyr Alputov <alputov1998@gmail.com>
2025-12-11 19:15:31 +0200

    fix: fixed font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ export default {
     extend: {
       fontFamily: {
         display: ["Playfair Display", "Georgia", "serif"],
-        sans: ["DM Sans", "system-ui", "sans-serif"],
+        sans: ["Roboto", "system-ui", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
